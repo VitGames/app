0e0187b5d65079734fdb2f90505ec5a371bff41c
дом <vitogrudov@gmail.com>
2021-03-22 19:05:42 +0300

    Home Task 2: Commit to request

--- app/src/main/java/io/techmeskills/an02onl_plannerapp/support/NavigationFragment.kt
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/support/NavigationFragment.kt
@@ -3,7 +3,6 @@ package io.techmeskills.an02onl_plannerapp.support
 import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import androidx.annotation.LayoutRes
-import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 
 abstract class NavigationFragment<T : ViewBinding>(@LayoutRes layoutResId: Int) :
@@ -18,13 +17,10 @@ abstract class NavigationFragment<T : ViewBinding>(@LayoutRes layoutResId: Int) 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requireActivity().onBackPressedDispatcher.addCallback(this, backPressedCallback)
-
     }
 
     override fun onDestroy() {
         super.onDestroy()
         backPressedCallback.remove()
     }
-
-
 }
